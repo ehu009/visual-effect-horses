4176dd11bd3689bf7a8bcf440e82e54509131d39
Eirik Husebø <jenssen.eirik@gmail.com>
2022-12-22 09:45:17 +0100

    horse constructor

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
 function forceTest() {
+}
+
+
+class Horse {
+    // a horse racing across the screen
+ 
+    constructor(id, xPos, yPos, scale, rotation, xDirection, yDirection, xSpeed, ySpeed) {
+        this.id = id;
+        this.x = xPos;
+        this.y = yPos;
+        this.s = scale;
+        this.r = rotation;
+        this.xd = xDirection;
+        this.yd = yDirection;
+        this.xv = xSpeed;
+        this.yv = ySpeed;
+        this.node = null;
+    }
     
 }
