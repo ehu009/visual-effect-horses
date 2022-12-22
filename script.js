00a47fae72c7b3cc2b00291a9ba58bc9b35922c6
Eirik Husebø <jenssen.eirik@gmail.com>
2022-12-22 09:55:45 +0100

    spawning and repositioning a horse

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function forceTest() {
 }
 
-
 class Horse {
     // a horse racing across the screen
  
@@ -19,4 +18,32 @@ class Horse {
         this.node = null;
     }
     
+    reposition() {
+        this.node.style.left = this.x;
+        this.node.style.top = this.y;
+    }
+
+    spawn() {
+        let img = document.createElement("img");
+        img.src = "example.jpg";
+        img.id = this.id;
+
+        img.style.height = "auto";
+        img.style.width = 150 * (this.s/100) + "px";
+        
+        //horf.style.width = +"";
+
+        img.classList.add("horse");
+        
+        if (this.xd > 0) {
+            img.classList.add("flipX");
+        }
+        if (0 > this.yd) {
+            img.classList.add("flipY");
+        }
+        document.getElementById("container").appendChild(img);
+        this.node = img;
+        this.reposition();
+        
+    }
 }
