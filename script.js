949cfbac7716e80fbe9a33d46d379743688001f6
Eirik Husebø <jenssen.eirik@gmail.com>
2022-12-25 14:36:46 +0100

    proper image positioning and orientation

--- script.js
+++ script.js
@@ -42,16 +42,62 @@ class Horse {
     }
     
     reposition() {
-        this.node.style.left = this.x;
-        this.node.style.top = this.y;
+        let x = 0;
+        let y = 0;
+        if (this.x > 0) {
+            x = window.innerWidth - this.node.width;
+        }
+        if (this.y > 0) {
+            y = window.innerHeight - this.node.height;
+        }
+        this.box.style.left = x;
+        this.box.style.top = y;
     }
 
     flip() {
-        if (this.xd > 0) {
-            this.node.classList.add("flipX");
-        }
-        if (0 > this.yd) {
-            this.node.classList.add("flipY");
+        if (this.y != 0) {
+            // horizontal movement
+            if (this.y < 0) {
+                // top
+                if (this.xd < 0) {
+                    // leftwards
+                    this.node.classList.add("flipY");
+                } else if (this.xd > 0) {
+                    // rightwards
+                    this.box.classList.add("rotate180");
+                }
+            } else if (this.y > 0) {
+                // bottom
+                if (this.xd < 0) {
+                    // leftwards
+                } else if (this.xd > 0) {
+                    // rightwards
+                    this.node.classList.add("flipX");
+                }
+            }
+        } else if (this.x != 0) {
+            // vertical movement
+            if (this.x < 0) {
+                // left
+                if (this.yd < 0) {
+                    // upwards
+                    this.box.classList.add("rotate90");
+                } else if (this.yd > 0) {
+                    // downwards
+                    this.node.classList.add("flipX");
+                    this.box.classList.add("rotate90");
+                }
+            } else if (this.x > 0) {
+                // right
+                if (this.yd < 0) {
+                    // upwards
+                    this.box.classList.add("rotate90");
+                    this.node.classList.add("flipY");
+                } else if (this.yd > 0) {
+                    // downwards
+                    this.box.classList.add("rotate270");
+                }
+            }
         }
     }
 
